4e1d784a4fd2b2d831a9ccee6f23d6d81560120b
18092397768 <522032242@qq.com>
2020-03-23 10:51:57 +0800

    使用useState转换Character组件中的state

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Summary from './Summary.jsx';
 // 角色模块
-class Character extends Component {
+const Character = props => {
     // loadedCharacter存放角色数据
-    state = {
-        loadedCharacter: {},
-        isLoading: false
-    }
-    // 当角色id、加载状态改变时，更新组件
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('shouldComponentUpdate');
-        return (
-            nextProps.selectedChar !== this.props.selectedChar ||
-            nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-            nextState.isLoading !== this.state.isLoading
-        );
-    }
-    // 当上一个角色id与现在的id不同时，请求数据
-    componentDidUpdate(prevProps) {
-        console.log('Component did update');
-        if (prevProps.selectedChar !== this.props.selectedChar) {
-            this.fetchData();
-        }
-    }
-    // 初次加载，请求数据
-    componentDidMount() {
-        this.fetchData();
-    }
+    const [loadedCharacter, setLoadedCharacter] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    // // 当角色id、加载状态改变时，更新组件
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('shouldComponentUpdate');
+    //     return (
+    //         nextProps.selectedChar !== props.selectedChar ||
+    //         nextState.loadedCharacter.id !== loadedCharacter.id ||
+    //         nextState.isLoading !== isLoading
+    //     );
+    // }
+    // // 当上一个角色id与现在的id不同时，请求数据
+    // componentDidUpdate(prevProps) {
+    //     console.log('Component did update');
+    //     if (prevProps.selectedChar !== props.selectedChar) {
+    //         this.fetchData();
+    //     }
+    // }
+    // // 初次加载，请求数据
+    // componentDidMount() {
+    //     this.fetchData();
+    // }
     // 根据id查找角色数据
-    fetchData = () => {
+    const fetchData = () => {
         console.log(
             'Sending Http request for new character with id ' + 
-            this.props.selectedChar
+            props.selectedChar
         );
         // 加载中
-        this.setState({ isLoading: true });
-        fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.selectedChar}`)
+        setIsLoading(true);
+        fetch(`https://pokeapi.co/api/v2/pokemon/${props.selectedChar}`)
         .then(response => {
             if(!response.ok) {
                 throw new Error('Could not fetch person!');
@@ -45,42 +43,42 @@ class Character extends Component {
         })
         .then(charData => {
             const loadedCharacter = {
-                id: this.props.selectedChar,
+                id: props.selectedChar,
                 name: charData.name,
                 height: charData.height,
                 abilities: charData.abilities,
                 baseExperience: charData.base_experience
             };
             // 存放角色信息、加载状态设置为加载完成
-            this.setState({ loadedCharacter: loadedCharacter, isLoading: false});
+            setLoadedCharacter(loadedCharacter);
+            setIsLoading(false);
         })
         .catch(err => {
             console.log(err);
+            setIsLoading(false);
         });
     };
-    // 组件即将卸载
-    componentWillUnmount() {
-        console.log('Too soon...');
-    }
+    // // 组件即将卸载
+    // componentWillUnmount() {
+    //     console.log('Too soon...');
+    // }
 
-    render() {
         let content = <p>Loading Character...</p>;
         // 已加载完成且角色id存在时
-        if (!this.state.isLoading && this.state.loadedCharacter.id) {
+        if (!isLoading && loadedCharacter.id) {
             content = (
                 <Summary
-                    name={this.state.loadedCharacter.name}
-                    gender={this.state.loadedCharacter.gender}
-                    height={this.state.loadedCharacter.height}
-                    baseExperience={this.state.loadedCharacter.baseExperience}
-                    abilities={this.state.loadedCharacter.abilities}
+                    name={loadedCharacter.name}
+                    gender={loadedCharacter.gender}
+                    height={loadedCharacter.height}
+                    baseExperience={loadedCharacter.baseExperience}
+                    abilities={loadedCharacter.abilities}
                 />
             );
-        } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
+        } else if (!isLoading && !loadedCharacter.id) {
             content = <p>Failed to fetch character.</p>;
         }
         return content;
-    }
 }
 
 export default Character;

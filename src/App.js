0ed2382465b677f13d3a140c6255bf245350ea37
18092397768 <522032242@qq.com>
2020-03-23 10:26:01 +0800

    使用 React Hooks 转换 App component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CharPicker from './components/CharPicker';
 import Character from './components/Character';
 
-class App extends Component {
-  // 状态有 角色id 主题颜色 是否摧毁
-  state = {
-    selectedCharacter: 1,
-    side: 'red',
-    destoryed: false
-  };
+const App = props => {
+  // 定义state，setState方法 和 初始值
+  const [selectedCharacter, setSelectCharacter] = useState(1);
+  const [side, setSide] = useState('red');
+  const [destoryed, setDestoryed] = useState(false);
+
   // 控制颜色
-  sideHandler = side => {
-    this.setState({ side: side });
+  const sideHandler = side => {
+    setSide(side);
   };
   // 控制角色选择
-  charSelectHandler = event => {
+  const charSelectHandler = event => {
     const charId = event.target.value;
-    this.setState({ selectedCharacter: charId });
+    setSelectCharacter(charId);
   };
   // 控制摧毁
-  destructionHandler = () => {
-    this.setState({ destoryed: true });
+  const destructionHandler = () => {
+    setDestoryed(true);
   };
 
-  render() {
     let content = (
       // <></>是<React.Fragment></React.Fragment>的语法糖
       <React.Fragment>
         {/* 角色选择模块 */}
         <CharPicker
-          side={this.state.side}
-          selectedChar={this.state.selectedCharacter}
-          onCharSelect={this.charSelectHandler}
+          side={side}
+          selectedChar={selectedCharacter}
+          onCharSelect={charSelectHandler}
         />
         {/* 角色展示模块 */}
-        <Character selectedChar={this.state.selectedCharacter} />
-        <button onClick={this.sideHandler.bind(this, 'red')}>
+        <Character selectedChar={selectedCharacter} />
+        <button onClick={sideHandler.bind(this, 'red')}>
           Red Light
         </button>
-        <button onClick={this.sideHandler.bind(this, 'blue')}>
+        <button onClick={sideHandler.bind(this, 'blue')}>
           Blue Light
         </button>
         {/* 颜色为蓝色时，显示摧毁按钮 */}
-        {this.state.side === 'blue' && (
-          <button onClick={this.destructionHandler}>DESTORY!</button>
+        {side === 'blue' && (
+          <button onClick={destructionHandler}>DESTORY!</button>
         )}
       </React.Fragment>
     );
     // 状态为已摧毁时，显示“全部摧毁”
-    if(this.state.destoryed) {
+    if(destoryed) {
       content = <h1>Total destruction!</h1>
     }
     return content;
-  }
 }
 
 export default App;

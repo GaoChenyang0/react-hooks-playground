0feb5a5fdc1ea2ae1b2aca55bd83acfc6b7c8ce1
18092397768 <522032242@qq.com>
2020-03-23 10:42:22 +0800

    格式化代码

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import CharPicker from './components/CharPicker';
-import Character from './components/Character';
+import React, { useState } from "react";
+import CharPicker from "./components/CharPicker";
+import Character from "./components/Character";
 
 const App = props => {
   // 定义state，setState方法 和 初始值
   const [selectedCharacter, setSelectCharacter] = useState(1);
-  const [side, setSide] = useState('red');
+  const [side, setSide] = useState("red");
   const [destoryed, setDestoryed] = useState(false);
 
   // 控制颜色
@@ -22,34 +22,30 @@ const App = props => {
     setDestoryed(true);
   };
 
-    let content = (
-      // <></>是<React.Fragment></React.Fragment>的语法糖
-      <React.Fragment>
-        {/* 角色选择模块 */}
-        <CharPicker
-          side={side}
-          selectedChar={selectedCharacter}
-          onCharSelect={charSelectHandler}
-        />
-        {/* 角色展示模块 */}
-        <Character selectedChar={selectedCharacter} />
-        <button onClick={sideHandler.bind(this, 'red')}>
-          Red Light
-        </button>
-        <button onClick={sideHandler.bind(this, 'blue')}>
-          Blue Light
-        </button>
-        {/* 颜色为蓝色时，显示摧毁按钮 */}
-        {side === 'blue' && (
-          <button onClick={destructionHandler}>DESTORY!</button>
-        )}
-      </React.Fragment>
-    );
-    // 状态为已摧毁时，显示“全部摧毁”
-    if(destoryed) {
-      content = <h1>Total destruction!</h1>
-    }
-    return content;
-}
+  let content = (
+    // <></>是<React.Fragment></React.Fragment>的语法糖
+    <React.Fragment>
+      {/* 角色选择模块 */}
+      <CharPicker
+        side={side}
+        selectedChar={selectedCharacter}
+        onCharSelect={charSelectHandler}
+      />
+      {/* 角色展示模块 */}
+      <Character selectedChar={selectedCharacter} />
+      <button onClick={sideHandler.bind(this, "red")}>Red Light</button>
+      <button onClick={sideHandler.bind(this, "blue")}>Blue Light</button>
+      {/* 颜色为蓝色时，显示摧毁按钮 */}
+      {side === "blue" && (
+        <button onClick={destructionHandler}>DESTORY!</button>
+      )}
+    </React.Fragment>
+  );
+  // 状态为已摧毁时，显示“全部摧毁”
+  if (destoryed) {
+    content = <h1>Total destruction!</h1>;
+  }
+  return content;
+};
 
 export default App;
